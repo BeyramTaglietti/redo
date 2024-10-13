9636a048f3d969ebdd67c28934ebb50032bcf54e
Beyram <beyram.taglietti@gmail.com>
2024-10-13 13:19:36 +0200

    feat: max amount of days set to 120

--- lib/features/timers/validation/timer.validation.ts
+++ lib/features/timers/validation/timer.validation.ts
@@ -4,7 +4,7 @@ export const TimerFormSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   days: z.coerce
     .number({ message: "Value must be a number" })
-    .max(60, { message: "Only a max of 60 days is allowed" })
+    .max(120, { message: "Only a max of 60 days is allowed" })
     .optional(),
   hours: z.coerce.number({ message: "Value must be a number" }).optional(),
   minutes: z.coerce.number({ message: "Value must be a number" }).optional(),
